36c4f606d635ea86aba11661375f746ddec9aca3
NMGVox <nijeemgreene@gmail.com>
2023-02-11 13:13:06 -0500

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if(str === ""){
+        return "";
+    }
+    
+    let rev_string = "";
+    for (let i = str.length-1; i >= 0; i--){
+        rev_string += str[i];
+    }
+    return rev_string;
 };
 
 // Do not edit below this line
