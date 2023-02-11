1f5128afd614a9bc4d4606a731520ed8a30ddf51
NMGVox <nijeemgreene@gmail.com>
2023-02-11 14:40:48 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    const temp = Array.from(arguments);
 
+    for(let i = 1; i < temp.length; i++){
+        const index = arguments[0].indexOf(temp[i]);
+        if( index === -1){
+            continue;
+        }
+        if (arguments[0][index] !== temp[i]){
+            continue;
+        }
+        arguments[0].splice(index, 1);
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
