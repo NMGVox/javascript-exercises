0bf591f157069ba0b2f97159f3af14194d2b077d
NMGVox <nijeemgreene@gmail.com>
2023-02-11 13:07:53 -0500

    Finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
 
+    let repeat = "";
+    while (num > 0){
+        repeat+=str;
+        num--;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
